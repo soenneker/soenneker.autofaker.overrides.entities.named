356015ec5e3b91fafa8b5600c684660eb0128381
Jake Soenneker <jake@soenneker.com>
2025-03-26 07:34:57 -0500

    Initial commit

---
+++ src/NamedEntityOverride.cs
@@ -0,0 +1,23 @@
+﻿using Soenneker.Entities.Named;
+using Soenneker.Utils.AutoBogus.Context;
+using Soenneker.Utils.AutoBogus.Override;
+
+namespace Soenneker.AutoFaker.Overrides.Entities.Named;
+
+/// <summary>
+/// An AutoFaker (AutoBogus) override for NamedEntity object
+/// </summary>
+public class NamedEntityOverrideOverride : AutoFakerOverride<NamedEntity>
+{
+    public override bool CanOverride(AutoFakerContext context)
+    {
+        return context.GenerateType.IsSubclassOf(typeof(NamedEntity));
+    }
+
+    public override void Generate(AutoFakerOverrideContext context)
+    {
+        var target = (context.Instance as NamedEntity)!;
+        target.Name = context.Faker.Commerce.ProductName();
+        target.Id = context.Faker.Random.Guid().ToString();
+    }
+}
---
+++ test/Soenneker.AutoFaker.Overrides.Entities.Named.Tests/NamedEntityOverrideTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.AutoFaker.Overrides.Entities.Named.Tests;
+
+public sealed class NamedEntityOverrideTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
