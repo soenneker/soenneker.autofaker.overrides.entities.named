e64574570715cb8c33bd5aa7523299a83b0f8ab4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:35:53 -0500

    xml commenting

--- src/Soenneker.AutoFaker.Overrides.Entities.Named/NamedEntityOverride.cs
+++ src/Soenneker.AutoFaker.Overrides.Entities.Named/NamedEntityOverride.cs
@@ -9,11 +9,20 @@ namespace Soenneker.AutoFaker.Overrides.Entities.Named;
 /// </summary>
 public class NamedEntityOverride : AutoFakerOverride<NamedEntity>
 {
+    /// <summary>
+    /// Executes the can override operation.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    /// <returns>A value indicating whether the operation succeeded.</returns>
     public override bool CanOverride(AutoFakerContext context)
     {
         return context.GenerateType.IsSubclassOf(typeof(NamedEntity));
     }
 
+    /// <summary>
+    /// Executes the generate operation.
+    /// </summary>
+    /// <param name="context">The context.</param>
     public override void Generate(AutoFakerOverrideContext context)
     {
         var target = (context.Instance as NamedEntity)!;
